4cc248bac2825d93a28b4d7be1b5b6bdd1eea5b0
BrunolBahiense <brunoluisbahiense@gmail.com>
2020-09-06 20:10:10 -0300

    criando app flappy bird

---
+++ 6.dom/js/flappy.js
@@ -0,0 +1,50 @@
+function novoElemento (tagName, className) {
+    const elem = document.createElement (tagName)
+    elem.className = className
+    return elem
+}
+
+function barreira (reversa = false) {
+
+    // sempre que se coloca o this.algumacoisa dentro de uma funçao, é para isso poder
+    //ser acessado também fora da função, para ser reaproveitado ao escrever o codigo 
+    this.elemento = novoElemento('div', 'barreira')
+
+    const borda = novoElemento('div', 'borda')
+    const corpo = novoElemento('div', 'corpo')
+    this.elemento.appendChild(reversa  ? corpo : borda)
+    this.elemento.appendChild(reversa  ? borda : corpo)
+
+    this.setAltura = altura => corpo.style.heith = `${altura}px`
+}
+
+/* const b = new barreira(true)
+b.setAltura(200)
+document.querySelector('[wm-flappy]').appendChild(b.elemento) */
+
+function parDeBarreiras (altura, abertura, x){
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new barreira(true)
+    this.inferior = new barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
+
+const b = new parDeBarreiras (700, 200, 400)
+document.querySelector('[wm-flappy]').appendChild(b.elemento)
