687208463c1e5fe6352a1e1f81bc65b256fa2f51
BrunolBahiense <brunoluisbahiense@gmail.com>
2020-09-24 18:36:19 -0300

    manipulando css pela dom

--- 9.webpack/webpack.config.js
+++ 9.webpack/webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
     output: {
         filename: 'principal.js',
         path: __dirname + '/public' //esse dirname aponta pro raiz, ou seja a pasta webpack
+    },
+    module: {
+        rules: [{
+            test: /\.css$/, // lendo arquivos .css
+            use: [
+                'style-loader', // adciona dentro da DOM a tag style
+                'css-loader',
+            ]
+        }]
     }
 }
