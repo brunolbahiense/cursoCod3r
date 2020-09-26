ac076cd634bcb5026a286a5a4f13d24b4c43d048
BrunolBahiense <brunoluisbahiense@gmail.com>
2020-09-26 11:20:56 -0300

    adcionandno development à build

--- 9.webpack/webpack.config.js
+++ 9.webpack/webpack.config.js
@@ -1,18 +1,33 @@
+const modoDev = process.env.NODE_ENV!== 'production'
 const webpack = require('webpack')
 const miniCssExtractPlugin = require('mini-css-extract-plugin') 
+const TerserPlugin = require('terser-webpack-plugin')
+const optimizeCssAssetsPluguin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production', // cria uma alternancia que escolhemos se estamos no desenvolvimento ou na produção 
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public' //esse dirname aponta pro raiz, ou seja a pasta webpack
     },
+    optimization: {
+        minimizer: [
+            new optimizeCssAssetsPluguin({})
+        ]
+
+    },
     plugins: [
         new miniCssExtractPlugin ({
             filename: "estilo.css"
 
-        })
+        }),
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        }),
     ],
     module: {
         rules: [{
